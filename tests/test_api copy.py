d2053ad159edbf9ea9e62d812e5fe6dd83950c40
itsdaudi <daudikombe77@gmail.com>
2026-07-04 19:49:38 +0300

    function to get single product

--- tests/test_api copy.py
+++ tests/test_api copy.py
@@ -21,3 +21,14 @@ def test_get_inventory(client):
     data = response.get_json()
 
     assert isinstance(data, list)
+
+#get single product route test
+def test_get_single_product(client):
+
+    response = client.get("/inventory/1")
+
+    assert response.status_code == 200
+
+    data = response.get_json()
+
+    assert data["id"] == 1
