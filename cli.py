2fdf46d26932699f84e192134be33ea2c3c835d8
itsdaudi <daudikombe77@gmail.com>
2026-07-04 13:27:03 +0300

    helper functions

--- cli.py
+++ cli.py
@@ -2,3 +2,12 @@
 import requests
 
 BASE_URL = "http://127.0.0.1:5555"
+#help function to print a separator line
+def print_separator():
+    print("\n" + "=" * 60)
+
+
+def handle_connection_error():
+    print("\nError: Could not connect to the Flask API.")
+    print("Make sure app.py is running on http://127.0.0.1:5555\n")
+
