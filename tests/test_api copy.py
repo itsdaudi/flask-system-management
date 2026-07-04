bc52e7029f7eceb1277dc81c105993f961de6f42
itsdaudi <daudikombe77@gmail.com>
2026-07-04 19:47:41 +0300

    getting all products route test function

--- tests/test_api copy.py
+++ tests/test_api copy.py
@@ -10,3 +10,14 @@ def test_home(client):
     data = response.get_json()
 
     assert "message" in data
+
+#get all products route test
+def test_get_inventory(client):
+
+    response = client.get("/inventory")
+
+    assert response.status_code == 200
+
+    data = response.get_json()
+
+    assert isinstance(data, list)
