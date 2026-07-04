c8a16d597d58a0e02939c3dd1fb540a1fb8ee2d7
itsdaudi <daudikombe77@gmail.com>
2026-07-04 12:40:34 +0300

    function to add product

--- app.py
+++ app.py
@@ -35,5 +35,49 @@ def get_product(item_id):
         "error": "Product not found"
     }), 404
 
+#add product to inventory
+@app.route("/inventory", methods=["POST"])
+def add_product():
+
+    data = request.get_json()
+
+    required_fields = [
+        "barcode",
+        "product_name",
+        "quantity",
+        "price"
+    ]
+
+    for field in required_fields:
+        if field not in data:
+            return jsonify({
+                "error": f"{field} is required."
+            }), 400
+
+    new_id = max(
+        [item["id"] for item in inventory],
+        default=0
+    ) + 1
+
+    new_product = {
+        "id": new_id,
+        "barcode": data["barcode"],
+        "product_name": data["product_name"],
+        "brand": data.get("brand", ""),
+        "category": data.get("category", ""),
+        "ingredients": data.get("ingredients", ""),
+        "quantity": data["quantity"],
+        "price": data["price"]
+    }
+
+    inventory.append(new_product)
+
+    return jsonify({
+        "message": "Product added successfully.",
+        "product": new_product
+    }), 201
+
+
+
 
 
