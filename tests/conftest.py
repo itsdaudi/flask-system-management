d7285762d5625071a911d49f10a11410a7931042
itsdaudi <daudikombe77@gmail.com>
2026-07-04 16:39:17 +0300

    confitest

---
+++ tests/conftest.py
@@ -0,0 +1,13 @@
+import pytest
+from app import app
+
+
+@pytest.fixture
+def client():
+    """
+    Creates a Flask test client for API testing.
+    """
+    app.config["TESTING"] = True
+
+    with app.test_client() as client:
+        yield client
